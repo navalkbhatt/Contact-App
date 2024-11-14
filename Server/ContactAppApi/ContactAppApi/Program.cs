7e5a88e8ce826af8e53de34e9b03e168ed49bf92
NavalKbhatt <navalkbhatt@hotmail.co.uk>
2024-11-15 01:38:06 +0530

    Change the docker file path

--- Server/ContactAppApi/ContactAppApi/Program.cs
+++ Server/ContactAppApi/ContactAppApi/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.WebHost.UseUrls("http://*:5000");
 //Add methods Extensions
 builder.Services.AddInjectionApplication();
 
